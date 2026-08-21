19ea4953c3c79349223d184c4aa2cfb1fde12472
discortakun1235-design <discortakun1235@gmail.com>
2026-08-22 05:47:01 +0700

    Add arena manager and command/event registrations

--- src/main/java/com/reyzmc/guardianvellr/GuardiansValler.java
+++ src/main/java/com/reyzmc/guardianvellr/GuardiansValler.java
@@ -1,16 +1,37 @@
 package com.namamu.guardiansvaller;
 
+import com.namamu.guardiansvaller.arena.ArenaManager;
+import com.namamu.guardiansvaller.commands.AdminCommand;
+import com.namamu.guardiansvaller.commands.DungeonCommand;
+import com.namamu.guardiansvaller.listeners.ArenaRuleListener;
+import com.namamu.guardiansvaller.listeners.BossCombatListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GuardiansValler extends JavaPlugin {
 
+    private ArenaManager arenaManager;
+
     @Override
     public void onEnable() {
-        getLogger().info("Guardians Valler has been enabled!");
+        saveDefaultConfig();
+
+        arenaManager = new ArenaManager();
+
+        getCommand("gv").setExecutor(new AdminCommand(this));
+        getCommand("dungeon").setExecutor(new DungeonCommand(this));
+
+        getServer().getPluginManager().registerEvents(new BossCombatListener(this), this);
+        getServer().getPluginManager().registerEvents(new ArenaRuleListener(this, arenaManager), this);
+
+        getLogger().info("GuardiansValler berhasil diaktifkan!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Guardians Valler has been disabled!");
+        getLogger().info("GuardiansValler telah dimatikan!");
+    }
+    
+    public ArenaManager getArenaManager() {
+        return arenaManager;
     }
 }
