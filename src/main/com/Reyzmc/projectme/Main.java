86c2652f5a442e9079fec58e59edb5d486707d05
discortakun1235-design <discortakun1235@gmail.com>
2026-08-27 21:13:32 +0700

    Implement plugin initialization and economy setup
    Initialize plugin, setup economy, and register commands.

--- src/main/com/Reyzmc/projectme/Main.java
+++ src/main/com/Reyzmc/projectme/Main.java
@@ -1,1 +1,41 @@
+package com.Reyzmc.projectme;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Economy econ = null;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        if (!setupEconomy()) {
+            getLogger().severe("Vault tidak ditemukan! Plugin dimatikan.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        getCommand("suit").setExecutor(new SuitCommand(this));
+        getServer().getPluginManager().registerEvents(new ChatListener(this), this);
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+    }
 
